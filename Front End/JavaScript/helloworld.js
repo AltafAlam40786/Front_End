4e0b504c934f68330bece2fe26b6fc14032b3180
Rockey Bhai <163630554+AltafAlam40786@users.noreply.github.com>
2025-11-29 14:36:58 +0530

    hy this is my new repo..

---
+++ Front End/JavaScript/helloworld.js
@@ -0,0 +1,12 @@
+console.log("hello world")
+console.log("hello world")
+// let a = 4
+// var a = 5
+a = 7
+b = 2
+console.log(a,b)
+console.log(a + b)
+console.log(a - b ) 
+console.log(a * b )  
+console.log(a / b ) 
+console.log(a % b ) 
